46bece5602264f64615d6d44de661717c77c515a
AlessandroCingolani <alessandro.cingolani93@gmail.com>
2024-02-27 12:27:44 +0100

    [add]:components

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
-<template>
-  <HeaderComponent />
-</template>
-
 <script>
 import HeaderComponent from "./components/HeaderComponent.vue";
+import PokedexComponent from "./components/PokedexComponent.vue";
 
 export default {
   name: "App",
   components: {
     HeaderComponent,
+    PokedexComponent,
   },
 };
 </script>
+
+<template>
+  <HeaderComponent />
+  <PokedexComponent />
+</template>
 
 <style lang="scss" scoped>
 @use "../scss/main.scss";

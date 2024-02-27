3f12d559ea5d04a9498d3722243a361e87721035
AlessandroCingolani <alessandro.cingolani93@gmail.com>
2024-02-27 15:18:39 +0100

    [add]:input search

---
+++ src/components/partials/PokemonSearch.vue
@@ -0,0 +1,23 @@
+<script>
+export default {
+  name: "PokemonSearch",
+};
+</script>
+
+<template>
+  <div
+    class="search-pokemon d-flex py-4 justify-content-between align-items-center"
+  >
+    <div class="h-100">
+      <input type="text" placeholder="Search Pokémon" />
+      <button class="btn btn-success ms-3" type="button">Button</button>
+    </div>
+    <button class="btn btn-success">Catch</button>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.search-pokemon {
+  height: 80px;
+}
+</style>

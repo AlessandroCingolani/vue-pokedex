a6a1d7c04e60cf8f4cbe43e6cb56f2e3a66cbb96
AlessandroCingolani <alessandro.cingolani93@gmail.com>
2024-02-27 12:27:56 +0100

    [add]:logo and border

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,12 +1,27 @@
-<template>
-  <h1>Header</h1>
-</template>
-
 <script>
 export default {
   name: "HeaderComponent",
 };
 </script>
 
+<template>
+  <header>
+    <div class="logo py-3">
+      <img src="/img/pokedex-logo.png" alt="" />
+    </div>
+  </header>
+</template>
+
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped></style>
+<style lang="scss" scoped>
+header {
+  border-bottom: 3px solid black;
+  .logo {
+    text-align: center;
+    img {
+      width: 200px;
+      height: 70px;
+    }
+  }
+}
+</style>

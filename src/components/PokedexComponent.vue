6e8e3c3947e1e6584ca288c358a6409cda66905a
AlessandroCingolani <alessandro.cingolani93@gmail.com>
2024-02-27 12:28:12 +0100

    [init]:pokedex component

---
+++ src/components/PokedexComponent.vue
@@ -0,0 +1,23 @@
+<script>
+export default {
+  name: "PokedexComponent",
+};
+</script>
+
+<template>
+  <div class="container">pokedex</div>
+</template>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+header {
+  border-bottom: 3px solid black;
+  .logo {
+    text-align: center;
+    img {
+      width: 200px;
+      height: 70px;
+    }
+  }
+}
+</style>

60c0f231dec02694773fff80632d6bce88235fe8
AlessandroCingolani <alessandro.cingolani93@gmail.com>
2024-02-28 12:26:39 +0100

    [add]:btn custome and emit catch

--- src/components/partials/PokemonSearch.vue
+++ src/components/partials/PokemonSearch.vue
@@ -17,18 +17,29 @@ export default {
       <input v-model="toSearch" type="text" placeholder="Search Pokémon" />
       <button
         @click="$emit('callApi', this.toSearch)"
-        class="btn btn-success ms-3"
+        class="ac_btn ms-3"
         type="button"
       >
-        Button
+        <i class="fa-solid fa-magnifying-glass"></i>
       </button>
     </div>
-    <button class="btn btn-success">Catch</button>
+    <button @click="$emit('catch')" class="ac_btn">Catch</button>
   </div>
 </template>
 
 <style lang="scss" scoped>
 .search-pokemon {
   height: 80px;
+  .ac_btn {
+    padding: 3px 10px;
+    border: none;
+    border-radius: 2px;
+    &:hover {
+      scale: 1.1;
+    }
+    &:active {
+      background-color: rgb(44, 212, 13);
+    }
+  }
 }
 </style>
